bb0a5fdfd6fa62b475cc4e5f579aa8f4a1194ed1
Abhishek kumar <abhishekku20022002@gmail.com>
2022-01-31 22:22:26 +0530

    changed some icons and functionality

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ const screen = document.querySelector(".screen");
 let expression = "";
 for (let button of buttons) {
 	button.addEventListener("click", (evt) => {
-		if (evt.target.innerText === "<") {
+		if (evt.target.innerText === "➥") {
 			expression = expression.slice(0, expression.length - 1);
 		} else if (evt.target.innerText === "C") {
 			expression = "";
 		} else if (evt.target.innerText === "=") {
 			expression = expression.replace("^", "**");
+			expression = expression.replace("÷", "/");
 			expression = eval(expression);
 			let num = parseFloat(expression);
 			num = num.toFixed(2);
